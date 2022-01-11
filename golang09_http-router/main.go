bfb0255bfdc8a63106430e9b063123e55c44fb32
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 14:03:48 +0700

    1. Route

--- golang09_http-router/main.go
+++ golang09_http-router/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func main() {
 	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "Hello HttpRouter")
+	})
 
 	server := http.Server{
 		Handler: router,
 		Addr:    "localhost:3000",
 	}
 
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
