ce5559f500ccacde0adb8f63eb0940311ae51ea2
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 22:34:37 +0700

    Category Repository Implementation

---
+++ golang11_restful_api/infrastructure/persistence/repository/category_repository_impl.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"golang11_restful_api/common"
+	"golang11_restful_api/domain/entity"
+)
+
+type CategoryRepositoryImpl struct{}
+
+func (c CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category *entity.Category) *entity.Category {
+	query := "INSERT INTO category(name) VALUES (?)"
+
+	result, err := tx.ExecContext(ctx, query, category.Name)
+	common.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	common.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
+}
+
+func (c CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category *entity.Category) *entity.Category {
+	query := "UPDATE category SET name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, query, category.Name, category.Id)
+	common.PanicIfError(err)
+
+	return category
+}
+
+func (c CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category *entity.Category) {
+	query := "DELETE FROM category where id = ?"
+	_, err := tx.ExecContext(ctx, query, category.Id)
+	common.PanicIfError(err)
+}
+
+func (c CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, id int) (*entity.Category, error) {
+	query := "SELECT FROM category WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, query, id)
+	common.PanicIfError(err)
+
+	category := entity.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category)
+		common.PanicIfError(err)
+		return &category, nil
+	} else {
+		return &category, errors.New("Category is not found")
+	}
+}
+
+func (c CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []entity.Category {
+	query := "SELECT * FROM category"
+	rows, err := tx.QueryContext(ctx, query)
+	common.PanicIfError(err)
+
+	var categories []entity.Category
+	for rows.Next() {
+		category := entity.Category{}
+		err := rows.Scan(&category)
+		common.PanicIfError(err)
+		categories = append(categories, category)
+	}
+
+	return categories
+}
