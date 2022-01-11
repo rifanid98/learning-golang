1e37d3bd695e0d084eda1714d555c109d1a3e155
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 14:30:23 +0700

    add descriptions

--- golang09_http-router/01router_test.go
+++ golang09_http-router/01router_test.go
@@ -10,6 +10,29 @@ import (
 	"testing"
 )
 
+/**
+Router
+
+- Inti dari library HttpRouter adalah struct Router
+- Router ini merupakan implementasi dari http.Handler, sehingga kita bisa dengan mudah menambahkan ke dalam http.Server
+- Untuk membuat Router, kita bisa menggunakan function httprouter.New(), yang akan mengembalikan Router pointer
+
+HTTP Method
+
+- Router mirip dengan ServeMux, dimana kita bisa menambahkan route ke dalam Router
+- Kelebihan dibandingakn dengan SeverMux adalah, pada Router kita bisa menentukan HTTP Method yang ingin kita gunakan.
+  Misalnya seperti GET, POST, PUT dan lain-lain
+- Cara menambahkan route ke dalam Router adalah dengan menggunakan function yang sama dengan HTTP Methodnya. Misal
+  router.GET(), router.POSTT() dan lain-lain
+
+httprouter.Handler
+
+- Saat kita menggunakan ServeMux, ketika menambahkan route, kita bisa menambahkan http.Handler
+- Berbeda dengan Router, pada Router kita tidak menggunakan http.Handler lagi, melainkan menggunakan type httprouter.Handle
+- Perbedaan dengan http.Handler adalah, pada httprouter.Handle, terdapat parameter ketiga yaitu Params
+
+*/
+
 func TestRouter(t *testing.T) {
 	router := httprouter.New()
 	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
--- golang09_http-router/02params_test.go
+++ golang09_http-router/02params_test.go
@@ -10,6 +10,19 @@ import (
 	"testing"
 )
 
+/**
+Params
+
+- httprouter.Handle memiliki parameter yang ketiga, yaitu Params. Untuk apa kegunaan Params?
+- Params merupakan tempat untuk meyimpan parameter yang dikirim dari client
+- Namun Params ini bukan query parameter, melainkan paramter di URL
+- kadang kita butuih membuat URL yang tidak fix, alias bisa berubah-ubah. Misalnya /products/1, /products/2 dan seterusnya
+- ServeMux tidak mendukung hal tersebut, namun Router mendukung hal tersebut
+- Parameter yang dinamis yang terdapat di URL, secara otomatis dikumpulkan di Params
+- Namun, agar Router tahu, kita harus memberi tahu ketika menambahkan Route, dibagian mana kita akan buat URL path nya
+  menjadi dinamis
+*/
+
 func TestParam(t *testing.T) {
 	router := httprouter.New()
 	router.GET("/products/:id", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
