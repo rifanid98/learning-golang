c9557b4f18d248a38388078d235cc9f7366856f0
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 23:15:12 +0700

    updated

--- golang11_restful_api/adapter/category_controller.go
+++ golang11_restful_api/adapter/category_controller.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-type CategoryHandler interface {
+type CategoryController interface {
 	Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
 	Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
 	Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
