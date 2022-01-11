b3d659126c5773bdf29b6d66b87815297b18a269
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 14:22:46 +0700

    pengenalan http router

---
+++ golang09_http-router/00httprouter.go
@@ -0,0 +1,16 @@
+package main
+
+/**
+Pengenalan HttpRouter
+
+- HttpRouter merupakan salah satu OpenSource Library yang populer untuk Http Handler di Go-Lang
+- HttpRouter terkenal dengan kecepatannya dan juga sangat minimalis
+- Hal ini dikarenakan HttpRouter hanya memiliki fitur untuk routing saja, tidak memiliki fitur apapun selain itu
+- https://github.com/julienschmidt/httprouter
+
+Menambah HttpRouter ke Project
+
+go get github.com/julienschmidt/httprouter
+go get github.com/stretchr/testify
+
+*/
