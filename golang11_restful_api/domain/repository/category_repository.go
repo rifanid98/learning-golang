36bdacf8fba84d5e696d1c33cbed45ffc36a1c1c
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-11 22:34:46 +0700

    refactored

--- golang11_restful_api/domain/repository/category_repository.go
+++ golang11_restful_api/domain/repository/category_repository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type CategoryRepository interface {
-	Save(ctx context.Context, tx sql.Tx, category *entity.Category) (*entity.Category, error)
-	Update(ctx context.Context, tx sql.Tx, category *entity.Category) (*entity.Category, error)
-	Delete(ctx context.Context, tx sql.Tx, category *entity.Category) error
-	FindById(ctx context.Context, tx sql.Tx, id int) (*entity.Category, error)
-	FindAll(ctx context.Context, tx sql.Tx) ([]entity.Category, error)
+	Save(ctx context.Context, tx *sql.Tx, category *entity.Category) *entity.Category
+	Update(ctx context.Context, tx *sql.Tx, category *entity.Category) *entity.Category
+	Delete(ctx context.Context, tx *sql.Tx, category *entity.Category)
+	FindById(ctx context.Context, tx *sql.Tx, id int) (*entity.Category, error)
+	FindAll(ctx context.Context, tx *sql.Tx) []entity.Category
 }
