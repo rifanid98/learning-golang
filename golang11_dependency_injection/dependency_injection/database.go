85cc633037c40cccefed09807d59b6ba8b26eb6f
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-01-12 17:19:43 +0700

    Multiple Binding

---
+++ golang11_dependency_injection/dependency_injection/database.go
@@ -0,0 +1,31 @@
+package dependency_injection
+
+type Database struct {
+	Name string
+}
+
+type DatabaseMongoDB Database
+type DatabasePostgreSQL Database
+
+func NewDatabaseMongoDB() *DatabaseMongoDB {
+	return (*DatabaseMongoDB)(&Database{Name: "MongoDB"})
+}
+
+func NewDatabasePostgreSQL() *DatabasePostgreSQL {
+	return (*DatabasePostgreSQL)(&Database{Name: "PostgreSQL"})
+}
+
+type DatabaseRepository struct {
+	DatabasePostgreSQL *DatabasePostgreSQL
+	DatabaseMongoDB    *DatabaseMongoDB
+}
+
+func NewDatabaseRepository(
+	databasePostgreSQL *DatabasePostgreSQL,
+	databaseMongoDB *DatabaseMongoDB,
+) *DatabaseRepository {
+	return &DatabaseRepository{
+		DatabasePostgreSQL: databasePostgreSQL,
+		DatabaseMongoDB:    databaseMongoDB,
+	}
+}
--- golang11_dependency_injection/dependency_injection/injector.go
+++ golang11_dependency_injection/dependency_injection/injector.go
@@ -20,3 +20,8 @@ func InitializedService(isError bool) (*SimpleService, error) {
 	wire.Build(NewSimpleRepository, NewSimpleService)
 	return nil, nil
 }
+
+func InitializedDatabaseRepository() *DatabaseRepository {
+	wire.Build(NewDatabasePostgreSQL, NewDatabaseMongoDB, NewDatabaseRepository)
+	return nil
+}
